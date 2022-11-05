b7399607348c7ef5ef132629ec334d02a28cb744
kaioruan <kaio.ruan@souunit.com.br>
2022-11-05 14:11:43 -0300

    fix: resolves bug in ambient variable to create and up migration / seeders database

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -2,16 +2,12 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  host: process.env.DB_HOST || '192.168.0.7',
+  username: process.env.DB_USER || 'sa',
+  password: process.env.DB_PASS || '12345678@',
   database: 'Desafio Fabritech',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  port: Number(process.env.DB_PORT) || 3004,
   dialect: 'mssql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
 }
 
 module.exports = config;
