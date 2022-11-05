ea302e04156f3f54659add94335ddbfcfb25484a
kaioruan <kaio.ruan@souunit.com.br>
2022-11-05 16:22:42 -0300

    feat: create route to get all clients

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import * as express from 'express';
 
 import UserController from './controller/User';
 import LoginValidation from './middlewares/LoginValidation';
+import AdminController from './controller/Admin';
 
 const userController = new UserController();
 const loginValidation = new LoginValidation();
+const adminController = new AdminController();
 class App {
   public app: express.Express;
 
@@ -17,6 +19,8 @@ class App {
     this.app.get('/', (req, res) => res.json({ ok: true }));
     this.app.post('/login', loginValidation.Validate, userController.login);
     this.app.post('/register', userController.Register);
+    this.app.post('/validate', userController.validate);
+    this.app.get('/admin', adminController.getAllClients);
   }
 
   private config():void {
