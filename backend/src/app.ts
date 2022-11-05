9f38bdba0871664edd5f34847e0cb72228af7e01
kaioruan <kaio.ruan@souunit.com.br>
2022-11-05 17:18:42 -0300

    feat: create middleware to validate registerUser and registerClient

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,12 @@ import * as express from 'express';
 import UserController from './controller/User';
 import LoginValidation from './middlewares/LoginValidation';
 import AdminController from './controller/Admin';
+import RegisterValidation from './middlewares/Register';
 
 const userController = new UserController();
 const loginValidation = new LoginValidation();
 const adminController = new AdminController();
+const registerValidation = new RegisterValidation();
 class App {
   public app: express.Express;
 
@@ -18,10 +20,10 @@ class App {
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
     this.app.post('/login', loginValidation.Validate, userController.login);
-    this.app.post('/register', userController.Register);
+    this.app.post('/register', registerValidation.ValidateUser, userController.Register);
     this.app.post('/validate', userController.validate);
     this.app.get('/admin', adminController.getAllClients);
-    this.app.post('/admin', adminController.createClient);
+    this.app.post('/admin', registerValidation.ValidateClient, adminController.createClient);
   }
 
   private config():void {
---
+++ backend/src/middlewares/Register.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from 'express';
+import ClientModel from '../database/models/ClientModel';
+
+class RegisterValidation {
+  public model = ClientModel;
+  public ValidateClient = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, username, address, role, relationship } = req.body;
+    const EMAIL_VALIDATION_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!email || !username || !address || !role || !relationship) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+
+    if (!EMAIL_VALIDATION_REGEX.test(email)) {
+      return res.status(400).json({ message: 'Email incorrect' });
+    }
+    const result = await this.model.findOne({ where: { email }, raw: true });
+    if (result) {
+      return res.status(401).json({ message: 'this email is already registered' });
+    }
+    next();
+  };
+
+  public ValidateUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, username, password, role } = req.body;
+    const EMAIL_VALIDATION_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email || !username || !password || !role) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    if (!EMAIL_VALIDATION_REGEX.test(email)) {
+      return res.status(400).json({ message: 'Email incorrect' });
+    }
+    const result = await this.model.findOne({ where: { email }, raw: true });
+    if (result) {
+      return res.status(401).json({ message: 'this email is already registered' });
+    }
+    if (password.length < 6) {
+      return res.status(401).json({ message: 'password must be at least 6 characters' });
+    }
+    if (username.length < 6) {
+      return res.status(401).json({ message: 'username must be at least 6 characters' });
+    } next();
+  };
+}
+
+export default RegisterValidation;
