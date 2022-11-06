356a905b1e3c2957bfd59153d801968db2a9dfba
kaioruan <kaio.ruan@souunit.com.br>
2022-11-06 15:19:56 -0300

    feat: create Header to page /admin

--- frontend/src/components/HeaderPage.vue
+++ frontend/src/components/HeaderPage.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
     <nav>
+      <p>Logo</p>
       <a href="/" alt="logout"> Sair </a>
     </nav>
   </header>
@@ -21,9 +22,13 @@ header {
 
 nav {
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: center;
   height: 100%;
+}
+p {
+  color: white;
+  font-size: 1.5rem;
 }
 a {
   color: white;
