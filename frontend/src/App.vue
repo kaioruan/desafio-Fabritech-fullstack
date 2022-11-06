69ce91c864d080d081b01e9be5fe3d69ddbd49d4
kaioruan <kaio.ruan@souunit.com.br>
2022-11-06 13:40:54 -0300

    setup initial frontend in vue.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HeaderPage />
+  </div>
+</template>
+
+<script>
+import "./styles/global.css";
+import HeaderPage from "./components/HeaderPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderPage,
+  },
+};
+</script>
---
+++ frontend/src/components/HeaderPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav>
+      <a href="/" alt="logout"> Sair </a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderPage",
+};
+</script>
+
+<style scoped>
+header {
+  background-color: var(--color-background-nav);
+  height: 60px;
+  width: 100%;
+}
+
+nav {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 100%;
+}
+a {
+  color: white;
+  font-size: 1.5rem;
+}
+</style>
