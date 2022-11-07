cc8b337d39bf26b7873d23733188c8dd91fd57fc
kaioruan <kaio.ruan@souunit.com.br>
2022-11-07 18:58:59 -0300

    feat: implement service to create in multi table and update

--- backend/src/services/AdminService.ts
+++ backend/src/services/AdminService.ts
@@ -9,7 +9,16 @@ class AdminService {
   public modelAddress = Address;
 
   public getAllClients = async (): Promise<IClient[]> => {
-    const result = await this.model.findAll({ raw: true });
+    const result = await this.model.findAll({
+      raw: true,
+      attributes: { exclude: ['address_id'] },
+      include: [
+        {
+          model: this.modelAddress,
+          as: 'address',
+        },
+      ],
+    });
     return result as unknown as IClient[];
   };
 
@@ -26,6 +35,34 @@ class AdminService {
       const resultClient = await this.model.create({
         email, username, role, relationship, addressId: result.id }, { transaction: t,
       });
+      return resultClient;
+    });
+    return transaction as unknown as IClientAddress;
+  };
+
+  public deleteClient = async (id: string): Promise<void> => {
+    const result = await this.model.findOne({ where: { id }, raw: true });
+    if (!result) {
+      throw new Error('User not found');
+    }
+    await this.model.destroy({ where: { id } });
+  };
+
+  public updateClient = async (id: string, client: IClientAddress): Promise<IClientAddress> => {
+    const { email, username, address, role, relationship, addressId } = client;
+    const { cep, street, country, district, city, state } = address;
+    const updateClient = { email, username, role, relationship };
+    const userAddress = { cep, street, country, district, city, state };
+    const transaction = await sequelize.transaction(async (t) => {
+      const result = await this.modelAddress.update(
+        { ...userAddress },
+        { where: { id: addressId }, transaction: t },
+      );
+      console.log(result);
+      const resultClient = await this.model.update(
+        { ...updateClient, id },
+        { where: { addressId }, transaction: t },
+      );
       return resultClient;
     });
     return transaction as unknown as IClientAddress;
