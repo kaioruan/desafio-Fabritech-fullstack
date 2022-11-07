c179b2fee8efa0919ad20cc360f841de6e536f0b
kaioruan <kaio.ruan@souunit.com.br>
2022-11-06 22:15:08 -0300

    feat: create service to insert values in table address and client

--- backend/src/services/AdminService.ts
+++ backend/src/services/AdminService.ts
@@ -1,18 +1,31 @@
+import IClientAddress from '../interface/IClientAddress';
 import IClient from '../interface/IClient';
 import ClientModel from '../database/models/ClientModel';
+import Address from '../database/models/AddressModel';
+import sequelize from '../database/models';
 
 class AdminService {
   public model = ClientModel;
+  public modelAddress = Address;
 
   public getAllClients = async (): Promise<IClient[]> => {
     const result = await this.model.findAll({ raw: true });
     return result as unknown as IClient[];
   };
 
-  public createClient = async (client: IClient): Promise<IClient> => {
-    const result = await this.model.create(client);
+  public createClient = async (client: IClientAddress): Promise<IClientAddress> => {
+    const { email, username, address, role, relationship } = client;
+    const { cep, street, country, district, city, state } = address;
+    const userAddress = { cep, street, country, district, city, state };
+    const transaction = await sequelize.transaction(async (t) => {
+      const result = await this.modelAddress.create({ ...userAddress }, { transaction: t });
+      const resultClient = await this.model.create({
+        email, username, role, relationship, addressId: result.id }, { transaction: t,
+      });
+      return resultClient;
+    });
 
-    return result as unknown as IClient;
+    return transaction as unknown as IClientAddress;
   };
 }
 
