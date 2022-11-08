31b2c766be1fdcd41951ce5316d74193370219ba
kaioruan <kaio.ruan@souunit.com.br>
2022-11-08 14:01:16 -0300

    feat: create mocks to test files

--- backend/src/tests/mocks/index.ts
+++ backend/src/tests/mocks/index.ts
@@ -18,3 +18,70 @@ export const UsersMock =  [
 export const token = Jwt.sign({ userId: 1 }, 'jwt_secret', {
   expiresIn: '1d',
 });
+
+export const ClientsMock = [
+  {
+    "id": 1,
+    "username": "Kaio Oliveira",
+    "email": "kaio@teste.com",
+    "role": "client",
+    "addressId": 17,
+    "relationship": "Pai/Mãe",
+    "address.id": 17,
+    "address.cep": "49090670",
+    "address.street": "Rua Antonio Freires",
+    "address.district": "Jardins",
+    "address.city": "Aracaju",
+    "address.state": "SE",
+    "address.country": "Brasil"
+  },
+  {
+    "id": 2,
+    "username": "Moura Oliveira",
+    "email": "moura@gmail.com",
+    "role": "client",
+    "addressId": 18,
+    "relationship": "Irmão/Irmã",
+    "address.id": 18,
+    "address.cep": "49090600",
+    "address.street": "Rua Jacy Andrade",
+    "address.district": "Soledade",
+    "address.city": "Aracaju",
+    "address.state": "SE",
+    "address.country": "Brasil"
+  }
+]
+
+export const newClient = {
+  "username": "Kaio God",
+  "email": "kaio@gmail.com",
+  "role": "client",
+  "relationship": "Pai",
+  "addressId": "2",
+  "address": {
+    "cep": "49090600",
+    "street": "Rua Jacy Andrade de Jesus",
+    "district": "Bugio",
+    "city": "Aracaju",
+    "state": "SE",
+    "country": "Brasil"
+  }
+}
+
+export const newClientInvalid = {
+  "username": "Kaio God",
+  "email": "kaio.com",
+  "role": "client",
+  "relationship": "Pai",
+}
+
+export const newClientAddressInvalid = {
+  "username": "Kaio God",
+  "email": "kaio@teste.com",
+  "role": "client",
+  "relationship": "Pai",
+  "address": {
+    "cep": "49090600",
+    "street": "Rua Jacy Andrade de Jesus",
+  }
+}
