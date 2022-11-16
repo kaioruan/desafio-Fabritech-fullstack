ae6a803f32a771425096e1afd4c77cedd2344933
kaioruan <kaio.ruan@souunit.com.br>
2022-11-16 09:31:12 -0300

    feat: insert more validate to middleware login validate

--- backend/src/middlewares/LoginValidation.ts
+++ backend/src/middlewares/LoginValidation.ts
@@ -5,12 +5,13 @@ class LoginValidation {
   public model = UserModel;
   public Validate = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
+    const EMAIL_VALIDATION_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!email) {
+    if (!email || !EMAIL_VALIDATION_REGEX.test(email)) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
 
-    if (!password) {
+    if (!password || password.length < 6) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
     const result = await this.model.findOne({ where: { email }, raw: true });
